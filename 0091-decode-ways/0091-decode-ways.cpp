7bae20fd78c96147d674ff2f6c085c8f0c6c2f5c
RAKSHITH H N <hnrakshith4@gmail.com>
2026-06-27 12:53:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (65.52%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
-    unordered_map<string,int> dp;
-
-    int solve(string &s, string curr, int i) {
-
-        string key = to_string(i) + "#" + curr;
-
-        if(dp.count(key))
-            return dp[key];
-
-        if(curr != ""){
-            int num = stoi(curr);
-
-            if(num < 1 || num > 26)
-                return dp[key] = 0;
-
-            if(i == s.size())
-                return dp[key] = 1;
+    int solve(string &s,int &n,int i,vector<int>&dp){
+        if(i==n)return 1;
+        if(s[i]=='0')return 0;
+        if(dp[i]!=-1)return dp[i];
+        int result=solve(s,n,i+1,dp);
+        if(i+1<n){
+            if(s[i]=='1'||s[i]=='2'&&s[i+1]<='6'){
+                result+=solve(s,n,i+2,dp);
+            }
         }
-
-        if(i >= s.size())
-            return dp[key] = 0;
-
-        int take = solve(s, curr + s[i], i + 1);
-
-        int split = 0;
-
-        if(curr != "")
-            split = solve(s, string(1,s[i]), i + 1);
-
-        return dp[key] = take + split;
+        return dp[i]=result;
     }
-
     int numDecodings(string s) {
-        if(s[0]=='0')
-            return 0;
-
-        return solve(s,"",0);
+        int n=s.size();
+        vector<int>dp(n,-1);
+        return solve(s,n,0,dp);
     }
 };
