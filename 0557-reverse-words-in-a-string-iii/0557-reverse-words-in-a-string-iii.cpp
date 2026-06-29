1ef7d3010a29337efcb46f5c3298957982cd6e25
RAKSHITH H N <hnrakshith4@gmail.com>
2026-06-29 14:20:45 +0530

    Time: 5 ms (28.55%), Space: 12.1 MB (89.47%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans = "";
         int n = s.size();
+        int x=0;
         for (int i = 0; i < n; i++) {
-            string temp="";
-            for (int j = i; j < n; j++) {
-                if (j == n - 1)
-                    temp += s[j];
-                if (s[j] == ' '||j==n-1) {
-                    reverse(temp.begin(), temp.end());
-                    ans += temp + " ";
-                    i=j;
-                    break;
-                }
-                else{
-                    temp+=s[j];
-                }
-            }
+           if(s[i]==' '){
+              reverse(s.begin()+x,s.begin()+i);
+              x=i+1;
+              i=x;
+           }
+           if(i==n-1){
+            reverse(s.begin()+x,s.end());
+           }
         }
-        ans.pop_back();
-        return ans;
+        return s;
     }
 };
