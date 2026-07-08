155c8df5c5eeae72c4a18ccaa881db2e006dae0d
RAKSHITH H N <hnrakshith4@gmail.com>
2026-07-08 14:29:41 +0530

    Time: 44 ms (53.37%), Space: 157.2 MB (44.04%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,64 +1,61 @@
 class Solution {
 public:
-    static const long long M = 1000000007;
+    typedef long long ll;
+    ll M = 1e9 + 7;
 
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
         int n = s.size();
 
-        vector<int> pos;
-        vector<long long> digit;
+        vector<ll> pref(n, 0);
+        vector<ll> sum(n, 0);
+        vector<int> cnt(n, 0);
+        vector<ll> pow10(n + 1, 1);
 
-        for (int i = 0; i < n; i++) {
-            if (s[i] != '0') {
-                pos.push_back(i);
-                digit.push_back(s[i] - '0');
-            }
-        }
-
-        int N = pos.size();
-
-        vector<long long> pow10(N + 1, 1);
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             pow10[i] = (pow10[i - 1] * 10) % M;
         }
 
-        vector<long long> prefNum(N + 1, 0);
-        for (int i = 0; i < N; i++) {
-            prefNum[i + 1] = (prefNum[i] * 10 + digit[i]) % M;
+        int d = s[0] - '0';
+
+        cnt[0] = (d != 0);
+        pref[0] = d;
+        sum[0] = d;
+
+        for (int i = 1; i < n; i++) {
+            d = s[i] - '0';
+
+            cnt[i] = cnt[i - 1] + (d != 0);
+
+            if (d != 0) {
+                pref[i] = (pref[i - 1] * 10 + d) % M;
+            } else {
+                pref[i] = pref[i - 1];
+            }
+
+            sum[i] = sum[i - 1] + d;
         }
 
-        vector<long long> prefSum(N + 1, 0);
-        for (int i = 0; i < N; i++) {
-            prefSum[i + 1] = prefSum[i] + digit[i];
-        }
+        vector<int> res;
 
-        vector<int> ans;
-
-        for (auto &q : queries) {
+        for (auto& q : queries) {
             int l = q[0];
             int r = q[1];
 
-            int L = lower_bound(pos.begin(), pos.end(), l) - pos.begin();
-            int R = upper_bound(pos.begin(), pos.end(), r) - pos.begin() - 1;
+            ll val= sum[r] - (l == 0 ? 0 : sum[l - 1]);
 
-            if (L > R) {
-                ans.push_back(0);
-                continue;
+            int k = cnt[r] - (l == 0 ? 0 : cnt[l - 1]);
+
+            ll x;
+
+            if (l == 0) {
+                x = pref[r];
+            } else {
+                x = (pref[r] - pref[l - 1] * pow10[k] % M + M) % M;
             }
 
-            int len = R - L + 1;
-
-            long long x =
-                (prefNum[R + 1] -
-                 prefNum[L] * pow10[len] % M +
-                 M) % M;
-
-            long long sum =
-                prefSum[R + 1] - prefSum[L];
-
-            ans.push_back((int)((x * (sum % M)) % M));
+            res.push_back((x * val) % M);
         }
 
-        return ans;
+        return res;
     }
 };
