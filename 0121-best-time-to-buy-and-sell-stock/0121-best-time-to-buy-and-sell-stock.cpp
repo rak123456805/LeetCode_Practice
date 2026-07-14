3f26e83c7efdb24b593a63c49671f41d86b86682
RAKSHITH H N <hnrakshith4@gmail.com>
2026-07-14 11:55:05 +0530

    Time: 0 ms (100%), Space: 97.2 MB (87.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,9 +5,7 @@ public:
         int maxprofit=0;
         int profit=0;
         for(int i=0;i<prices.size();i++){
-            if(minValue>prices[i]){
-                minValue=prices[i];
-            }
+            minValue=min(minValue,prices[i]);
             profit=prices[i]-minValue;
             maxprofit=max(maxprofit,profit);
         }
