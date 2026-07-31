fe5651e8b6f299e1c9ac5c7c16510a6690a82c6e
RAKSHITH H N <hnrakshith4@gmail.com>
2026-07-31 12:02:39 +0530

    Time: 2 ms (15.78%), Space: 10.5 MB (5.72%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n=word.size();
-        int assign=2;
-        unordered_map<int,int>mp;
-        int ans=0;
-        for(auto &it :word){
-            if(assign>9){
-                assign=2;
-            }
-            mp[assign]++;
-            ans+=mp[assign];
-            assign++;
+        int n = word.size();
+        vector<int> freq(26, 0);
+        for (auto& it : word) {
+            freq[it - 'a']++;
+        }
+        int ans = 0;
+        sort(freq.rbegin(), freq.rend());
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0)
+                break;
+            ans += freq[i] * (i / 8 + 1);
         }
         return ans;
     }
