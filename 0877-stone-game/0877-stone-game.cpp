888e064bd906e541eb31142cc1b0a871a52de391
RAKSHITH H N <hnrakshith4@gmail.com>
2026-08-02 22:41:17 +0530

    Time: 20 ms (9%), Space: 19.7 MB (11.06%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -6,8 +6,8 @@ public:
             return 0;
         if (dp[i][j] != -1)
             return dp[i][j];
-        int left = piles[i] + solve(piles, i + 1, j, dp);
-        int right = piles[j] + solve(piles, i, j - 1, dp);
+        int left = piles[i] - solve(piles, i + 1, j, dp);
+        int right = piles[j] - solve(piles, i, j - 1, dp);
         return dp[i][j] = max(left, right);
     }
     bool stoneGame(vector<int>& piles) {
