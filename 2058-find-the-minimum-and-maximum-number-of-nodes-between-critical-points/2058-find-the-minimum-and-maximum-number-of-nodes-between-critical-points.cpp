e93e7e91723d045d7b0d8198a4f347ceaec53625
RAKSHITH H N <hnrakshith4@gmail.com>
2026-08-31 10:53:58 +0530

    Time: 8 ms (26.86%), Space: 134.1 MB (5.02%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,39 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> ans;
-
-        ListNode* temp = head;
-
-        while (temp != nullptr) {
+        ListNode *temp=head;
+        vector<int>ans;
+        while(temp->next!=NULL){
             ans.push_back(temp->val);
-            temp = temp->next;
+            temp=temp->next;
         }
-
-        if (ans.size() <= 2)
-            return {-1, -1};
-
-        int first = -1;
-        int prev = -1;
-        int minDist = INT_MAX;
-
-        for (int i = 1; i < ans.size() - 1; i++) {
-            if ((ans[i] > ans[i - 1] && ans[i] > ans[i + 1]) ||
-                (ans[i] < ans[i - 1] && ans[i] < ans[i + 1])) {
-
-                if (first == -1) {
-                    first = i;
-                } else {
-                    minDist = min(minDist, i - prev);
+        ans.push_back(temp->val);
+        int first=-1;
+        int prev=-1;
+        int minDist=INT_MAX;
+        for(int i=1;i<ans.size()-1;i++){
+            if((ans[i]>ans[i-1]&&ans[i]>ans[i+1])||(ans[i]<ans[i-1]&&ans[i]<ans[i+1])){
+                if(first==-1){
+                    first=i;
                 }
-
-                prev = i;
+                else{
+                    minDist=min(minDist,i-prev);
+                }
+                prev=i;
             }
         }
-
-        if (first == -1 || first == prev)
-            return {-1, -1};
-
-        return {minDist, prev - first};
+        if(first==-1 || first==prev)return {-1,-1};
+        return {minDist,prev-first};
     }
 };
