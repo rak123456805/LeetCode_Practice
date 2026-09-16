a61d37a2327d5cf13809331cdccb754bb39b8c7a
RAKSHITH H N <hnrakshith4@gmail.com>
2026-09-16 12:56:55 +0530

    Time: 6 ms (72.43%), Space: 9.9 MB (75.7%) - LeetHub

---
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -0,0 +1,46 @@
+#define ll long long
+class Solution {
+public:
+    vector<ll> fact;
+    vector<ll> invFact;
+    const int MOD = 1e9+7;
+
+    //Binary Exponentiation - Taught here - https://www.youtube.com/watch?v=D320QeHS0XQ
+    ll findPower(ll a, ll b) {
+        if(b == 0)
+            return 1;
+        
+        ll half = findPower(a, b/2);
+        ll result = (half * half) % MOD;
+
+        if(b%2 == 1) {
+            result = (result * a) % MOD;
+        }
+
+        return result;
+    }
+
+    //nCr % MOD
+    ll nCr(int n, int r) {
+        //nCr = n! * inv(n-r)! * inv(r)!
+        return (((fact[n] * invFact[r]) % MOD) * invFact[n-r]) % MOD;
+    }
+
+    int numberOfSets(int n, int k) {
+        //Precompute factoriala and inverse factorials - required for Fermat's Little Theorem
+        fact.assign(n+k, 1);
+        invFact.assign(n+k, 1);
+
+        for(int i = 2; i < n+k; i++) {
+            fact[i] = (fact[i-1] * i) % MOD;
+        }
+
+        //Fermat's little theorem for inverse factorials
+        //Taught here - https://www.youtube.com/watch?v=FMBW7m1Wap0
+        for(int i = 0; i < n+k; i++) {
+            invFact[i] = findPower(fact[i], MOD-2);
+        }
+
+        return nCr(n+k-1, 2*k);
+    }
+};
